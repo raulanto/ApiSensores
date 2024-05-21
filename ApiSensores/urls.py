8687f42de4c82d1744400d05438e55cd131a0e9d
raul <74162376+raulanto@users.noreply.github.com>
2024-05-21 14:43:45 -0600

    cambios
    cambios

--- ApiSensores/urls.py
+++ ApiSensores/urls.py
@@ -43,7 +43,7 @@ urlpatterns = [
                   path('api/v1/', include('Equipo.api.urls')),
                   path('api/v1/', include('Proceso.api.urls')),
                   path('registro/', views.registro_usuario, name='registro'),
-                  path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
                   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
